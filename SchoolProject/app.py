de13515fa6f1517b44ae24f623c2925811c25eeb
arnavsrivastava-edu <arnavsri0101@gmail.com>
2025-02-10 11:54:33 +0530

    Update app.py

--- SchoolProject/app.py
+++ SchoolProject/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import h2o
 app = Flask(__name__)
 h2o.init()
-model = h2o.load_model('/Users/arnavsrivastava/PycharmProjects/SchoolProject/model/StackedEnsemble_AllModels_1_AutoML_1_20250131_222515')
+model = h2o.load_model('model/StackedEnsemble_AllModels_1_AutoML_1_20250131_222515')
 @app.route('/')
 def home():
     return render_template('index.html')
